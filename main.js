740594a40e9a6eaa308b1b0ceeda11358bc8ea76
阳光正好 <taozi@yangguangzhenghaodeMacBook-Air.local>
2020-01-28 14:35:05 +0800

    2020/1/28 14:35:00 入门electron跟着课程敲的代码

--- main.js
+++ main.js
@@ -1,53 +1,57 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const DataStore = require('./renderer/musicDataStore');
+const myStore = new DataStore({name: 'music'})
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const finalConfig = { ...{
+      width: 800,
+      height: 600,
+      show: false,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    }, ...config }
+    super(finalConfig);
+    this.loadFile(fileLocation);
+    this.once('ready-to-show', () => { // 页面内容渲染完后才显示
+      this.show();
+    })
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html');
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.send('getTracks', myStore.getTracks())
+  })
+  ipcMain.on('add-music-window', () => {
+    const addWindow = new AppWindow({
+      width: 500,
+      height: 400,
+      parent: mainWindow
+    }, './renderer/add.html');
+  })
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  ipcMain.on('open-music-file', (event) => {
+    dialog.showOpenDialog({
+      properties: ['openFile', 'multiSelections'],
+      filters: [{name: 'Music', extensions: ['mp3']}]
+    }).then(files => {
+      if (files) {
+        event.sender.send('selected-files', files.filePaths)
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  })
+
+  ipcMain.on('add-tracks', (event, tracks) => {
+    const updatedTracks = myStore.addTracks(tracks).getTracks()
+    mainWindow.send('getTracks', updatedTracks)
+  }) 
+
+  ipcMain.on('delete-track', (event, id) => {
+    const updatedTracks = myStore.deleteTrack(id).getTracks();
+    mainWindow.send('getTracks', updatedTracks)
+  })
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
---
+++ renderer/helper.js
@@ -0,0 +1,10 @@
+exports.$ = (id) => {
+    return document.getElementById(id);
+}
+
+exports.convertDuration = time => {
+    // 计算分钟
+    const minutes = "0" + Math.floor(time / 60)
+    const seconds = "0" + Math.floor(time - minutes * 60)
+    return minutes.substr(-2) + ":" + seconds.substr(-2)
+}
